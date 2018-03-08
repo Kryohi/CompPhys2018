1c6946dee7295a4a0b00af75372b5fa6c01af01f
Kryohi <fabio96.ck@gmail.com>
2018-03-08 21:24:05 +0100

    Generalized plots improvements, new density plot

--- 2-VOK_Neutron_Stars/LaneEmden_NeutronStar.jl
+++ 2-VOK_Neutron_Stars/LaneEmden_NeutronStar.jl
@@ -1,7 +1,5 @@
 
-using QuadGK, Plots, Rsvg
-#pyplot()
-Plots.plotlyjs()
+using QuadGK, Plots, LaTeXStrings
 
 function integrateAll(arg, h)
     s1 = sum(arg[3:2:end-2])
@@ -24,52 +22,68 @@ function RK4system(t::Float64, fx, fy, x::Float64, y::Float64, h=1e-3)
     return [x, y]
 end
 
-h = 1e-4
-Umax = 6.0
-u = linspace(h, Umax, ceil(Umax/h))
-ϕ = zeros(ceil(Umax/h))
-θ = zeros(ceil(Umax/h))
-
-## Initial conditions
-ϕ[1] = 0.0
-θ[1] = 1.0
-
 ## solution of the differential system
 # u is called ξ on paper
-function solveLaneEmden(n::Float64=2.2)  # n is the polytropic index
+function solveLaneEmden(u, n=2.2, h=1e-3)  # n is the polytropic index, u the radius array
+    umax = u[end]
+    ϕ = zeros(ceil(umax/h))
+    θ = zeros(ceil(umax/h))
+
+    # Initial conditions
+    ϕ[1] = 0.0
+    θ[1] = 1.0
+
+    # Differential system
     f1(u_,phi) = -phi/u_^2
     f2(u_,theta) = u_^2*theta^n
-    #for i = 1:length(u)-1
+
+    # Evaluate the differential equations until θ reaches 0 (almost)
     i = 1
-    # Evaluate the differential equations until θ reaches 0
-    while θ[i] > 5e-4
+    while θ[i] > 1e-4
         θ[i+1], ϕ[i+1] = RK4system(u[i], f1, f2, θ[i], ϕ[i], h)
-        if i > (length(u)-10) println("i = ", i, "  θ = ", θ[i+1], "  ϕ = ", ϕ[i+1]) end
         i+=1
     end
     return [θ, ϕ, u[i]] # where u[i] is the adimensional radius found
 end
-θ, ϕ, umax = solveLaneEmden(2.1)
 
-## Plots
-p1 = plot(u[θ.!=0], θ[θ.!=0])   # plotta ϕ e θ all'interno della stella
-plot!(u[θ.!=0], ϕ[θ.!=0])
-savefig(p1,"thetaphi.svg")
+## plot θ e ρ inside the star for different ns
+function densityProfiles(nn)
+    #gr()   #faster than pgfplots()
+    pgfplots() #xaxis=(0.0,Umax), yaxis=(0.0,1.05) plot!(u[θ.!=0], ϕ[θ.!=0])
+    h = 5e-4
+    Umax = 6.0  # maximum adimensional radius
+    u = linspace(h, Umax, ceil(Umax/h))
+    θ, ϕ, umax = solveLaneEmden(u, nn[1], h)
+    p1 = plot(u[1:5:end], θ[1:5:end], xlab=L"\xi", ylab=L"\theta", label=latexstring("n = ",nn[1]), guidefontsize=30, xaxis=((0.0, 6.01)))
+    p2 = plot(u[1:5:end], θ[1:5:end].^nn[1], xlab=L"\xi", ylab=L"\rho", label=latexstring("n = ",nn[1]), guidefontsize=30)
+    for n ∈ nn[2:end]
+        θ, ϕ, umax = solveLaneEmden(u, n, h)
+        plot!(p1, u[1:5:end], θ[1:5:end], label=latexstring("n = ",n))
+        plot!(p2, u[1:5:end], θ[1:5:end].^n, label=latexstring("n = ",n))
+    end
+    Plots.savefig(p1,"theta.pdf")    #better to use with the pgfplots() backend
+    Plots.savefig(p2,"density.pdf")    #better to use with the pgfplots() backend
+end
+
+densityProfiles(1.5:0.1:2.5)
 
 ## Find the radius and mass of the star for a range of n
 function radiusMass(nmin, nmax)
     U = []  #raggio adimensionale
     M = []  #massa adimensioanle
+    h = 5e-4
+    Umax = 6.0  # maximum adimensional radius
+    u = linspace(h, Umax, ceil(Umax/h))
     for n = nmin:0.05:nmax
-        θ, ϕ, umax = solveLaneEmden(n)
+        θ, ϕ, umax = solveLaneEmden(u, n, h)
         push!(U, umax)   #  mette in un array il raggio adimensionale raggiunto
-        push!(M, 4π*integrateAll(θ.*u.^2, 1e-4))
+        push!(M, 4π*integrateAll(θ.^n.*u.^2, h))
     end
     return [U,M]
 end
-U, M = radiusMass(1.5, 3.5)
-p2 = plot(U,M)
-savefig(p2,"aradiusamass.svg")
+U, M = radiusMass(1.5, 2.5)
+p3 = Plots.plot(U, M, xaxis=("Adimensional radius",(3.65,5.5)), yaxis=("Adimensional mass",(26,36)), legend=false, guidefontsize=30)
+savefig(p3,"aradiusamass.pdf")
 
 
 ## Conversion to physical units
