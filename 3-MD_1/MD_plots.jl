fa3b8d58299c9579411edadd51658fab39960e46
Kryohi <fabio96.ck@gmail.com>
2018-04-03 15:16:28 +0200

    minor adjustments

--- 3-MD_1/MD_plots.jl
+++ 3-MD_1/MD_plots.jl
@@ -3,7 +3,6 @@ if nprocs()<4
   addprocs(4)
 end
 
-
 @everywhere include("/home/kryohi/Uni/CompPhys2018/3-MD_1/Periodic_Gas.jl")
 push!(LOAD_PATH, "/home/kryohi/Uni/CompPhys2018/3-MD_1")
 #@everywhere reload("Sim")
@@ -27,12 +26,12 @@ end
     #N = 108
     #T0 = 0.5
     # Use a small fstep (even 1) for the PV plot, but higher (20-50) to create the animation
-    println("Run ", rho, "/", 2.4)
-    XX, EE, TT, PP, = Sim.simulation(N=N, T0=T0, rho=rho, maxsteps=6*10^4, fstep=30, dt=5e-4, anim=false, csv=false, onlyP=false)
+    println("Run ", rho, "/", 3.4)
+    XX, EE, TT, PP, = Sim.simulation(N=N, T0=T0, rho=rho, maxsteps=1*10^5, fstep=5, dt=5e-4, anim=false, csv=false, onlyP=false)
     P, dP = avgAtEquilibrium(PP)  #+ ρ[i]*TT[length(PP)÷4:end])
     E, dE = avgAtEquilibrium(EE)
     T, dT = avgAtEquilibrium(TT)
-    Sim.make2DtemporalPlot(XX[:,1:1700], T=T0, rho=rho, save=true)
+    #Sim.make2DtemporalPlot(XX[:,1:1700], T=T0, rho=rho, save=true)
     return P, dP, E, dE, T, dT
 end
 
@@ -50,7 +49,7 @@ DP = convert(DataFrame, [ρ V P])
 file = string("./3-MD_1/Data/PV_",N,"_T",T0,".csv")
 #CSV.write(file, DP)
 
-rV1 = plot(ρ, P, xaxis=("ρ",(0,ρ[end])), yaxis=("P",(0,ceil(P[end]))), linewidth=2, leg=false)
+rV1 = plot(ρ, P, ribbon=dP, xaxis=("ρ",(0,ρ[end])), yaxis=("P",(0,ceil(P[end]))), linewidth=2, leg=false)
 file = string("./3-MD_1/Plots/rV_",N,"_T",T0,".pdf")
 #savefig(rV1,file)
 
