16b9601c0f2480148040f23fe1720d4dcd01d6b0
Kryohi <fabio96.ck@gmail.com>
2018-04-24 18:16:18 +0200

    better order parameter, minor plot corrections

--- 3-MD_1/MD_plots.jl
+++ 3-MD_1/MD_plots.jl
@@ -13,7 +13,10 @@ PyPlot.PyObject(PyPlot.axes3D)  # servirà finché non esce la prossima versione
 fnt = "sans-serif"
 default(titlefont=Plots.font(fnt,24), guidefont=Plots.font(fnt,24), tickfont=Plots.font(fnt,14), legendfont=Plots.font(fnt,14))
 
-@everywhere function avgAtEquilibrium(A, f=4)   # where f is the fraction of steps to cut off
+# where f is the fraction of steps to cut off
+# per sistemi già abbastanza all'equilibrio (e.g. densità alta) anche 4-5 va bene
+# altrimenti (T bassa, ρ bassa) meglio 3 o anche 2 (se serve 2 meglio aumentare maxsteps)
+@everywhere function avgAtEquilibrium(A, f=3)
     l = length(A)
     return mean(A[l÷f:end]), std(A[l÷f:end])/sqrt(l*(1-1/f))
 end
@@ -21,13 +24,13 @@ end
 @everywhere function parallelPV(rho, N, T0, rhoarray)
     # Use a small fstep (even 1) for the PV plot, but higher (20-50) to create the animation
     println("Run ", find(rhoarray.==rho), "/", length(rhoarray))
-    XX, EE, TT, PP, CM = Sim.simulation(N=N, T0=T0, rho=rho, maxsteps=1*10^5, fstep=10, dt=5e-4,
-     anim=false, csv=false, onlyP=false)
+    XX, EE, TT, PP, CM = Sim.simulation(N=N, T0=T0, rho=rho, maxsteps=12*10^4,
+     fstep=20, dt=5e-4, anim=false, csv=false, onlyP=false)
     P, dP = avgAtEquilibrium(PP)  #+ ρ[i]*TT[length(PP)÷4:end])
     E, dE = avgAtEquilibrium(EE)
     T, dT = avgAtEquilibrium(TT)
     #ld = Sim.lindemann2(XX, CM, N, rho)
-    op = Sim.orderParameter(XX, N, rho)
+    op = Sim.orderParameter(XX, rho)
     #Sim.make2DtemporalPlot(XX[:,1:1700], T=T0, rho=rho, save=true)
     return P, dP, E, dE, T, dT, op
 end
@@ -37,8 +40,8 @@ end
 ##
 
 ρ = 0.075:0.025:1.125
-N = 108
-T0 = 7.0
+N = 256
+T0 = 1.7
 V = N./ρ
 
 # map the parallelPV function to the ρ array
@@ -56,11 +59,12 @@ rename!(DP, f => t for (f, t) = zip([:x1, :x2, :x3, :x4, :x5, :x6, :x7, :x8, :x9
 file = string("./Data/PV_",N,"_T",T0,".csv")
 CSV.write(file, DP)
 
-rV1 = plot(ρ, P, ribbon=dP, fillalpha=.3, xaxis=("ρ",(0,ρ[end])), yaxis=("P",(0,ceil(P[end]))), linewidth=2, leg=false)
+rV1 = plot(ρ, P, ribbon=dP, fillalpha=.3, xaxis=("ρ",(0,ceil(ρ[end]*10)/10)),
+ yaxis=("P",(0,ceil(P[end]))), linewidth=2, leg=false)
 file = string("./Plots/rV_",N,"_T",T0,".pdf")
 savefig(rV1,file)
 
-PV1 = plot(V, P, ribbon=dP, fillalpha=.3, xaxis=("V",(0,2000)), yaxis=("P",(0,ceil(P[end]))), linewidth=2, leg=false)
+PV1 = plot(V, P, ribbon=dP, fillalpha=.3, xaxis=("V",(0,2000)), yaxis=("P",(-1,ceil(P[end]))), linewidth=2, leg=false)
 file = string("./Plots/PV_",N,"_T",T0,".pdf")
 savefig(PV1,file)
 
@@ -68,9 +72,9 @@ gui()
 
 
 ## prove varie
-#XX, EE, TT, PP, CM = Sim.simulation(N=108, T0=1.0, rho=0.9, maxsteps=5*10^4, fstep=10, dt=5e-4, anim=false, csv=true)
+#XX, EE, TT, PP, CM = Sim.simulation(N=256, T0=1.0, rho=0.8, maxsteps=16*10^4, fstep=80, dt=5e-4, anim=false, csv=false)
 # @show Sim.avg3D(CM)
 # Sim.make2DtemporalPlot(XX[:,100:200], T=1.0, rho=0.4, save=true)
 # Sim.make3Dplot(CM, T=1.0, rho=1.3)
 # ld = Sim.lindemann2(XX, CM, 108, 1.1)
-#OP = Sim.orderParameter(XX, 108, 0.1)
+#OP = Sim.orderParameter(XX, 0.05)
