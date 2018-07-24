392d0c6bb5fe3231f3412cf6dceb4913af5a0128
Kryohi <fabio96.ck@gmail.com>
2018-07-24 17:49:07 +0200

    better numbers, wnd=maxsteps/10

--- 4-MonteCarlo_1/MC_main.jl
+++ 4-MonteCarlo_1/MC_main.jl
@@ -13,10 +13,8 @@ import MC
 
 #@time XX, jj, je = HO.oscillators(N=1000, D=3.5, T0=10.0, maxsteps=2030)
 
-@time XX, EE, PP, je, jj, C_H, CV, CV2 = MC.metropolis_ST(N=108, T=1.5,
- rho=0.2, maxsteps=80000, fstep=1, Df=1/70)
-
-#@time E, P, CV, je, jj = MC.metropolis_MP(N=500, T=2.0, rho=0.1, maxsteps=2000, Df=1/50)
+@time XX, EE, PP, je, jj, C_H, CV, CV2 = MC.metropolis_ST(N=108, T=1.2,
+ rho=0.2, maxsteps=150000, fstep=1, Df=1/70)
 
 
 ##
@@ -26,7 +24,7 @@ import MC
 @everywhere function parallelPV(rho, N, T, Tarray)
     println("Run ", find(Tarray.==T)[1], "/", length(Tarray))
 
-    XX, EE, PP, je, jj, C_H, CV, CV2 = MC.metropolis_ST(N=N, T=T, rho=rho, maxsteps=100000, fstep=1, Df=1/60)
+    XX, EE, PP, je, jj, C_H, CV, CV2 = MC.metropolis_ST(N=N, T=T, rho=rho, maxsteps=150000, fstep=1, Df=1/60)
 
     E, dE = mean(EE), std(EE)
     P, dP = mean(PP), std(PP)
@@ -42,7 +40,7 @@ N = 108
 V = N./ρ
 
 # # map the parallelPV function to the ρ array
- @time result = pmap(T0 -> parallelPV(ρ, N, T0, T), T)
+@time result = pmap(T0 -> parallelPV(ρ, N, T0, T), T)
 
 # extract the resulting arrays from the result tuple
 P, dP = [ x[1] for x in result ], [ x[2] for x in result ]
