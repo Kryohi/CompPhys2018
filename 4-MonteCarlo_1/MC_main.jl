804f709ff10663e85ecd5c1694544e5bb2579ea2
Kryohi <fabio96.ck@gmail.com>
2018-10-13 15:32:02 +0200

    add rho and T print

--- 4-MonteCarlo_1/MC_main.jl
+++ 4-MonteCarlo_1/MC_main.jl
@@ -23,7 +23,8 @@ nprocs()<4 && addprocs(4)   # add local worker processes (N is the num of logica
     # Df iniziale andrebbe ottimizzato anche per T
     EE, PP, jj, C_H, CV, CV2, OP = MC.metropolis_ST(N=N, T=T, rho=rho, maxsteps=24*10^6, Df=(1/100)) #1/75 per ρ=0.01
 
-    @info string("Run ", findfirst(Tarray.==T), " finished, with tau = ", sum(C_H))
+    @info string("Run ", findfirst(Tarray.==T),
+    " finished, with tau = ", sum(C_H), " (T = ", T, "rho = ", rho, ")")
     E, dE = mean(EE), std(EE)   # usare variance2?
     P, dP = mean(PP), std(PP)
     τ = sum(C_H)
